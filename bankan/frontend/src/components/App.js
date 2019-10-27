02833e3a18512e49d96bbe4c2909fd468737ab93
hupratt <hp@hp.com>
2019-10-27 07:29:26 +0100

    first try at react state

--- bankan/frontend/src/components/App.js
+++ bankan/frontend/src/components/App.js
@@ -1,12 +1,25 @@
 import ReactDOM from "react-dom";
 import React, { Component } from "react";
+import {
+  MovableCardWrapper,
+  CardHeader,
+  CardRightContent,
+  CardTitle,
+  Detail,
+  Footer
+} from "../styles/Base";
 
 export default class App extends Component {
+  // static state = { "0": { title: "hello", completed: "false" } };
+  state = { title: "hello", completed: "false" };
+  static url = "https://jsonplaceholder.typicode.com/todos";
   render() {
     return (
-      <div>
-        <h1>Hello</h1>
-      </div>
+      <MovableCardWrapper>
+        <CardHeader> {this.state.title} </CardHeader>
+        <Detail>hELLO</Detail>
+        <Footer />
+      </MovableCardWrapper>
     );
   }
 }
